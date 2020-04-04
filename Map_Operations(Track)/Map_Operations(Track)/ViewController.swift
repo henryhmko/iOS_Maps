9de71577db6f9456d5d8ee7e96cbcaeba3ed83e7
Henry Ko <ko.hyeonmok@berkeley.edu>
2020-04-04 21:55:14 +0900

    Restarted new tracker

---
+++ Map_Operations(Track)/Map_Operations(Track)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Map_Operations(Track)
+//
+//  Created by Hyeonmok Ko on 2020/04/04.
+//  Copyright © 2020 Hyeonmok Ko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
